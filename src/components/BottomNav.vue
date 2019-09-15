abc1af2e5a5194fdb6f59dfa13707699a63140fe
quantum design <tests8q@gmail.com>
2019-09-15 19:21:21 +0100

    initial commit; end of part 1

---
+++ src/components/BottomNav.vue
@@ -0,0 +1,30 @@
+
+<template>
+
+  <v-bottom-nav app fixed :value="true">
+
+    <v-btn flat color="primary" :to="{ path: '/'}">
+      <span>Top Stories</span>
+      <v-icon>mdi-thumb-up-outline</v-icon>
+    </v-btn>
+
+    <v-btn flat color="primary" :to="{ name: 'code-examples'}">
+      <span>Code Examples</span>
+      <v-icon>mdi-code-braces</v-icon>
+    </v-btn>
+
+    <v-btn flat color="primary" :to="{ name: 'my-favorites'}">
+      <span>Favorites</span>
+      <v-icon>mdi-lamp</v-icon>
+    </v-btn>
+
+  </v-bottom-nav>
+
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+  @Component
+  export default class BottomNav extends Vue {
+  }
+</script>
