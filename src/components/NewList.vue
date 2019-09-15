b5b65555c3cfcbe97adeea79117441f20072a661
quantum design <tests8q@gmail.com>
2019-09-15 19:39:01 +0100

    added new decorator to news list component

--- src/components/NewList.vue
+++ src/components/NewList.vue
@@ -1,27 +1,26 @@
-
 <template>
   <div>
 
     <v-container fluid grid-list-lg>
       <v-layout row wrap>
-        <v-flex xs12>
+        <v-flex xs12 v-for="newsArticle in newsArticles" :key="newsArticle.id">
           <v-card>
             <v-container fluid grid-list-lg>
               <v-layout row>
                 <v-flex xs9>
                   <div>
                     <div class="subheading font-weight-medium">
-                      Vue.js is the absolute rockstar of the JavaScript ecosystem
+                      {{ newsArticle.title }}
                     </div>
-                    <div>Jan 22 2019</div>
+                    <div>{{ newsArticle.dateString }}</div>
                   </div>
                 </v-flex>
                 <v-flex xs3>
                   <v-img
                     contain
-                    src="/thumbnails/winner.png"
-                    srcset="/thumbnails/winner.png 1x,
-                            /thumbnails/winner@2x.png 2x"
+                    :src="`/thumbnails/${newsArticle.baseImageName}.png`"
+                    :srcset="`/thumbnails/${newsArticle.baseImageName}.png 1x,
+                             /thumbnails/${newsArticle.baseImageName}@2x.png 2x`"
                     height="72px"
                     transition="false"
                   ></v-img>
@@ -36,3 +35,12 @@
 
   </div>
 </template>
+
+<script lang="ts">
+  import { Component, Prop, Vue } from 'vue-property-decorator';
+  import { NewsArticle } from '../types';
+  @Component
+  export default class NewsList extends Vue {
+    @Prop({required: true}) newsArticles!: NewsArticle[];
+  }
+</script>
